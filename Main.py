ecb966b91ff2d69a70cfdb23d5ed29da5fe11a99
Mário Victor Ribeiro Silva <mario.victor@unifesspa.edu.br>
2018-11-28 15:28:51 -0300

    Adicionada função que esconde a turtle

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import turtle
-import time
+import random as rand
+import math
 
 tela = turtle.Screen()  # Instancia a tela (Não necessario caso n mudar atributos)
 tela.bgcolor('black')    # Define a cor da tela
@@ -8,16 +9,28 @@ tela.setup(1200, 720)    # Define o tamanho da janela
 pen = turtle.Pen()   # Constrói um objeto caneta  (Pen)
 pen.hideturtle()     # Esconde a Turtle
 
-pen.shape('classic') # Muda a forma do cursor/caneta
+pen.speed(0)
 pen.color('white')
-pen.forward(100)     # anda 100 posições para a frente
-pen.right(45)        # gira o ponteiro em 45 graus para a direita
-pen.forward(100)     # anda mais 100 posições para a frente
-pen.circle(50)       # Desenha um circulo de raio 50
+
+# Função para adicionar ceu estrelado
+for i in range(200):
+    x = rand.randint(-800, 800)
+    y = rand.randint(-360, 360)
+
+    if math.sqrt(pow(x, 2) + pow(y, 2)) > 200:
+        pen.up()
+        pen.goto(x, y)
+        pen.down()
+        pen.dot(2, 'white')
+
+pen.up()
+pen.goto(0, -200)
+pen.down()
+pen.circle(200)       # Desenha um circulo de raio 50
 
 pen.up()             # Levanta o 'Rabo' para parar de desenhar
-pen.goto(20, 20)     # Indica uma coordenada a ir
-pen.dot(5, 'white')  # Desenha um ponto de tamanho 5 e cor branca
+pen.goto(0, 0)     # Indica uma coordenada a ir
+pen.dot(2, 'white')  # Desenha um ponto de tamanho 5 e cor branca
 pen.goto(0, 0)
 pen.down()           # Abaixa o 'Rabo'
 
