a923f64d1a1892c112d3ed89297a59e0396b3b3b
Mário Victor Ribeiro Silva <mario.victor@unifesspa.edu.br>
2018-11-28 22:45:59 -0300

    Comentario sobre a função que gera p circulo "manualmente"

--- Main.py
+++ Main.py
@@ -3,6 +3,9 @@ import random as rand
 import math
 
 # usar #000080 para azul com sobra e #0000ff para azul normal
+# Apesar de existir uma função para desenhar o circulo, precisamos percorre-lo
+
+raio = 200
 
 tela = turtle.Screen()  # Instancia a tela (Não necessario caso n mudar atributos)
 tela.bgcolor('black')    # Define a cor da tela
@@ -21,7 +24,7 @@ for i in range(500):
     x = rand.randint(-800, 800)
     y = rand.randint(-360, 360)
 
-    if math.sqrt(pow(x, 2) + pow(y, 2)) > 200:
+    if math.sqrt(pow(x, 2) + pow(y, 2)) > raio:
         pen.up()
         pen.goto(x, y)
         pen.down()
@@ -31,12 +34,17 @@ for i in range(500):
 
 tela.update() # Faz update da tela, pois a animação está desligada
 pen.up()
-pen.goto(0, -200)
+pen.goto(0, raio)
 pen.down()
-pen.circle(200)       # Desenha um circulo de raio 50
+
+passo = math.pi * raio / 180
+
+for j in range(360):
+    pen.forward(passo)
+    pen.right(1)
 
 pen.up()             # Levanta o 'Rabo' para parar de desenhar
-pen.goto(0, 0)     # Indica uma coordenada a ir
+pen.goto(0, 0)       # Indica uma coordenada a ir
 pen.color("#000080")
 pen.dot(10)
 
